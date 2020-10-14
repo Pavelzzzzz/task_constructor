6ea227c785f1fbbe0bf07d733bd9db32911d80e7
Pavelzzzzz <Pavelzzzzz1@gmail.com>
2020-10-14 16:37:19 +0300

    server init

---
+++ spring/src/main/java/com/pavelzzzzzz/TaskControlApplication.java
@@ -0,0 +1,13 @@
+package com.pavelzzzzzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskControlApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/com/pavelzzzzzz/TaskControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavelzzzzzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
